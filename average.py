17692f7ab9eeabcbe1507b1a229003106641617f
abdhu-omy <abdhullak149@gmail.com>
2021-07-05 19:45:29 +0530

    this is some changes in the files,in performancd

--- average.py
+++ average.py
@@ -1,7 +1,7 @@
-number1 = input( "Enter three numbers" )
-number2 = input()
-number3 = input()
+number1 = int(input( "Enter three numbers" ) )
+number2 = int( input() )
+number3 = int( input() )
 
-sum =( int( number1 ) + int( number2 ) + int( number3 ) ) / 3
+sum =( number1 + number2 + number3 ) / 3
 
 print( "Result is :"+str( sum ) )
