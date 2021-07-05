7f1416c11aa84cd8541193f71b62914259193163
abdhu-omy <abdhullak149@gmail.com>
2021-07-05 19:38:11 +0530

    first python commit , there is two files

---
+++ Hello.py
@@ -0,0 +1,5 @@
+a = input( "Enter two numbers" )
+b = input()
+print( "Hello " +str( a ) + " Hi " +str( b ) )
+sum = ( int( a ) +int( b ) )
+print( "Sum is: "+str( sum ) )
---
+++ average.py
@@ -0,0 +1,7 @@
+number1 = input( "Enter three numbers" )
+number2 = input()
+number3 = input()
+
+sum =( int( number1 ) + int( number2 ) + int( number3 ) ) / 3
+
+print( "Result is :"+str( sum ) )
