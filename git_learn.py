d5c05e4cd907ba0df5cfb8bf206a49b68f39c1d0
abdhulla-k <abdhullak149@gmail.com>
2021-07-30 13:06:58 +0530

    studys about git

---
+++ git_learn.py
@@ -0,0 +1,57 @@
+"""
+                                                     GIt
+
+1. git configer ( it is using ror add or change user name or email )
+
+2. git init ( It is using for initializing git in a directory)
+
+3. git add  or git add --all  or git add .  or git add <Enter specific file name you want to add in a commit>  ( it is 
+   using to add files for commit. cummit includes only added files or changes)
+4. git commit -m "Enter message here" ( this is the cummit format)
+
+5. git status
+
+6. git log or git log --all or git log --all --decorate  or git log --all --decorate --graph (usig to see the cummit details)
+
+7. git diff ( it shows all differences between last cummit and current edited file)
+
+8. git branch <name> (for create a new branch)
+
+9. git branch (it will show the current branch)
+
+10. git merge <name> (it is using to merge between two branches)   ( there are two types of merges availabe.one is 
+    fast forward merge and another one is requaseem merge. requaseem merge is a merging method that merging between two 
+    branches that twoo are editod.in this situation,maybe ther is a conflict.then you can mannually edit the files and 
+    commit after that)
+
+11. git remote <link> for adding a remote
+
+12. git remote (for rename remote)
+
+13. git remote <link> (for cheqe or cee the link added to remote)
+
+14. git push <remote name> <brabcn naem> (for push to server)
+
+15. git cclone <link> (for add an existing project from server to your computer)
+
+16. git pull ( it is using for pull the current edited code from server to your computer for cheque the matching with 
+    your main branch)
+
+17. git fetch (it is same as 'git pull'.but in this case the code will come to your remote and you want to merge it to 
+    your main. but in the case of 'git pull',it will come directlly to your main branch)
+
+==========================================================================
+
+                                        Team work
+
+how to add a new member in to your team:
+             
+             open the depository > settings > manage access > invite (serch for a person) >invite outside colaburate >
+             select access mode > then add
+
+If you are invite in to a team.firstlly you can clone the repository in to your computr and create a new branch.then edit 
+or write in that nelly created branch.after the editing prossoss, pull the current code and merge main in to working 
+branch.then cheque the codes and assure the match with and currect conflict.then push working branch in to server.
+then go to the repository and create a new pull requast as main branch as base and working branch as compare .
+
+"""
